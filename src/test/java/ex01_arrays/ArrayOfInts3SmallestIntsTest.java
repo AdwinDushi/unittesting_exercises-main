ef642d1d1cae20d471c8b8cbbe773bbba6595aad
AdwinDushi <r0749250@student.thomasmore.be>
2023-09-18 08:49:15 +0200

    Initial commit

---
+++ src/test/java/ex01_arrays/ArrayOfInts3SmallestIntsTest.java
@@ -0,0 +1,12 @@
+package ex01_arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayOfInts3SmallestIntsTest {
+
+    @Test
+    void sumOf3SmallestInts() {
+    }
+}
